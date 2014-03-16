a217a3d3180d4f88a0ea8c718a3d07a8ddba57c1
proft <proftua@gmail.com>
2014-03-16 20:20:43 +0200

    send alert about new comment

--- ineo/views.py
+++ ineo/views.py
@@ -5,6 +5,9 @@ from django.views.generic.edit import CreateView
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.core.mail import send_mail
+
 from ineo.models import Comment
 from ineo.forms import CommentAddForm
 
@@ -21,7 +24,15 @@ class CommentAddView(CreateView):
     template_name = 'ineo/comment_add.html'
 
     def form_valid(self, form):
-        form.save()
+        comment = form.save()
+
+        if hasattr(settings, 'EMAIL_RECIPIENTS'):
+            sender = settings.EMAIL_FROM
+            subject = u"%s новый отзыв для %s" % (settings.EMAIL_SUBJECT_PREFIX, comment.content_object)
+            message = u"Имя: %s\nEMail: %s\n\n%s\n\nУправление комментарием" % (comment.name,
+                comment.email, comment.comment, reverse('admin:ineo_comment_change', args=[comment.id]))
+            send_mail(subject, message, sender, settings.EMAIL_RECIPIENTS)
+
         messages.info(self.request, u'Спасибо, Ваш комментарий добавлен!')
         return HttpResponseRedirect(self.get_success_url())
 
