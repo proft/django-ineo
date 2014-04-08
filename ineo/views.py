b237b0c347dd36a0e854e475d398b86740752260
proft <proftua@gmail.com>
2014-04-09 00:18:20 +0300

    encode for message text

--- ineo/views.py
+++ ineo/views.py
@@ -105,7 +105,7 @@ class IneoReplyView(FormView):
             subject = u"%s новый отзыв для %s" % (settings.EMAIL_SUBJECT_PREFIX, comment.content_object)
             message = u"Объект: %s\nИмя: %s\nEMail: %s\n\n%s\n\nУправление комментарием: %s" % (comment.content_object,
                 comment.name, comment.email, comment.comment, edit_url)
-            send_mail(subject, message, sender, settings.EMAIL_RECIPIENTS)
+            send_mail(subject, message.encode('utf-8'), sender, settings.EMAIL_RECIPIENTS)
 
         messages.info(self.request, u'Спасибо, Ваш комментарий добавлен!')
         self.url_kwargs = {}
